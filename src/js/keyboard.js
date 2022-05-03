d3c2b758aafa928414d82b2580c83c4359ef06a8
piwe2004 <piwe2004@gmail.com>
2022-05-03 17:15:35 +0900

    가상 키보드 최종

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,26 +1,99 @@
 export class Keyboard{
     #switchEl;
     #fontSelectEl;
+    #containerEl;
+    #keyboardEl;
+    #inputGroupEl;
+    #inputEl;
+    #keyPress = false;
+    #mouseDown = false;
     constructor() {
         this.#assignElement();
         this.#addEvent();
     }
 
     #assignElement(){
-        this.#switchEl = document.getElementById("switch");
-        this.#fontSelectEl = document.getElementById("font");
+        this.#containerEl = document.getElementById("container");
+        this.#switchEl = this.#containerEl.querySelector("#switch");
+        this.#fontSelectEl = this.#containerEl.querySelector("#font");
+        this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+        this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+        this.#inputEl = this.#inputGroupEl.querySelector("#input");
     }
 
     #addEvent(){
-        this.#switchEl.addEventListener("change", (e) => {
-            document.documentElement.setAttribute(
-                "theme",
-                e.target.checked ? "dark-mode" : ""
-            );
-        });
-        this.#fontSelectEl.addEventListener("change", (e) =>{
-            //document.body.childNodes().style.fontFamily = e.target.value;
-            document.getElementById("input").style.fontFamily = e.target.value;
-        });
+        this.#switchEl.addEventListener("change", this.#onChangeTheme);
+        this.#fontSelectEl.addEventListener("change", this.#onchangeFont);
+        document.addEventListener("keydown", this.#onKeyDown.bind(this));
+        document.addEventListener("keyup", this.#onKeyUp.bind(this));
+        this.#inputEl.addEventListener("input", this.#onInput);
+        this.#keyboardEl.addEventListener("mousedown", this.#onMouseDown.bind(this));
+        document.addEventListener("mouseup", this.#onMouseUp.bind(this));
+    }
+
+    #onMouseUp(e){
+        if(this.#keyPress) return;
+        this.#mouseDown = false;
+
+        const keyEl = e.target.closest("div.key");
+        const isActive = !!keyEl?.classList.contains("active");
+        const val = keyEl?.dataset.val;
+        if(isActive && !!val && val !== "Space" && val != "Backspace"){
+            this.#inputEl.value += val;
+        }
+
+        if(isActive && val === "Space"){
+            this.#inputEl.value += " ";
+        }
+
+        if(isActive && val === "Backspace"){
+            this.#inputEl.value = this.#inputEl.value.slice(0,-1);
+        }
+
+        this.#keyboardEl.querySelector(".active")?.classList.remove("active");
+    }
+
+    #onMouseDown(e){
+        if(this.#keyPress) return;
+        this.#mouseDown = true;
+        e.target.closest("div.key")?.classList.add("active");
+    }
+
+    #onInput(e){
+        e.target.value = e.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+    }
+
+    #onKeyDown(e){
+        if(this.#mouseDown) return;
+        this.#keyPress = true;
+
+        this.#keyboardEl
+            .querySelector(`[data-code=${e.code}]`)
+            ?.classList.add("active");
+
+        this.#inputGroupEl.classList.toggle(
+            "error",
+            /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(e.key)
+        );
+    }
+
+    #onKeyUp(e){
+        if(this.#mouseDown) return;
+        this.#keyPress = false;
+
+        this.#keyboardEl
+            .querySelector(`[data-code=${e.code}]`)
+            ?.classList.remove("active");
+    }
+
+    #onChangeTheme(e){
+        document.documentElement.setAttribute(
+            "theme",
+            e.target.checked ? "dark-mode" : ""
+        );
+    }
+
+    #onchangeFont(e){
+        document.getElementById("input").style.fontFamily = e.target.value;
     }
 }
